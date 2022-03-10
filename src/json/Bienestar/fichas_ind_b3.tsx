808e237cf7b9accbccf9fda5baa66a2287b49674
rafaelcanchola <jcancholaa1300@alumno.ipn.mx>
2022-03-10 17:46:41 -0600

    fix info

--- src/json/Bienestar/fichas_ind_b3.tsx
+++ src/json/Bienestar/fichas_ind_b3.tsx
@@ -1,7 +1,7 @@
 export const ind_b3 = {
     "Nombre": "1.3 Número de acciones de financiamiento y subsidios de vivienda para personas en condiciones de vulnerabilidad",
     "Objetivo prioritario": "Garantizar el ejercicio del derecho a la vivienda adecuada a todas las personas, especialmente a los grupos en mayor condición de discriminación y vulnerabilidad, a través de soluciones financieras, técnicas y sociales de acuerdo con las necesidades específicas de cada grupo de población",
-    "Definición o descripción": "Medición del número de acciones de financiamiento o subsidios otorgadas a personas de población indígena,  mujeres y adultos mayores (más de 60 años). Este indicador permitirá dar seguimiento a la asequibilidad de las viviendas con base en el número de personas en condiciones de vulnerabilidad que reciben apoyos públicos para vivienda. Las modalidades de financiamiento para este cálculo incluyen créditos individuales, cofinanciamiento, subsidios, entre otros, otorgados principalmente por los ONAVIS (CONAVI, FOVISSSTE, INFONAVIT, SHF) y la Banca Privada. Estas acciones pueden ser para autoproducción, contragarantías, lote con servicios, mejoramiento, reconstrucción, renta, acceso a vivienda nueva y vivienda existente",
+    "Definición o descripción": "Medición del número de acciones de financiamiento o subsidios otorgadas a personas de población indígena,  mujeres y adultos mayores (más de 60 años). Este indicador permitirá dar seguimiento a la asequibilidad de las viviendas con base en el número de personas en condiciones de vulnerabilidad que reciben apoyos públicos para vivienda. Las modalidades de financiamiento para este cálculo incluyen créditos individuales, cofinanciamiento, subsidios, entre otros, otorgados principalmente por las ONAVIS (CONAVI, FOVISSSTE, INFONAVIT, SHF) y la Banca Privada. Estas acciones pueden ser para autoproducción, contragarantías, lote con servicios, mejoramiento, reconstrucción, renta, acceso a vivienda nueva y vivienda existente",
     "Nivel de desagregación": "Nacional",
     "Periodicidad o frecuencia de medición": "Anual",
     "Tipo": "Gestión",
@@ -12,28 +12,20 @@ export const ind_b3 = {
     "Disponibilidad de la información": "Diciembre",
     "Tendencia esperada": "Ascendente",
     "Unidad Responsable de reportar el avance":  "15.- Desarrollo Agrario, Territorial y Urbano\n511 Dirección General de Desarrollo Urbano, Suelo y Vivienda",
-    "Método de cálculo": "El aumento en acciones de subsidios y financiamiento para soluciones de vivienda se medirá basado en el número de acciones de subsidios y financiamientos para las poblaciones vulnerables seleccionadas, el cuál es publicado en el SNIIV\nAcciones de subsidios y financiamiento de poblaciones vulnerables= X1+X2+X3\nDonde: X1=Subsidios y financiamiento a hombres de municipios indígenas\nX2= Subsidios y financiamiento a mujeres (de todas las edades)\nX3=Subsidios y financiamiento a hombres que no residen en municipios indígenas en el rango de edad de 60 o más.",
+    "Método de cálculo": "El aumento en acciones de subsidios y financiamiento para soluciones de vivienda se medirá basado en el número de acciones de subsidios y financiamientos para las poblaciones vulnerables seleccionadas, el cuál es publicado por el SNIIV\nAcciones de subsidios y financiamiento de poblaciones vulnerables= X1+X2+X3\nDonde: X1=Subsidios y financiamiento a hombres de municipios indígenas\nX2= Subsidios y financiamiento a mujeres (de todas las edades)\nX3=Subsidios y financiamiento a hombres que no residen en municipios indígenas en el rango de edad de 60 o más.",
     "Observaciones": "La selección de las variables se determinó para evitar duplicados en el cálculo, de manera que se abarcan todas las poblaciones vulnerables seleccionadas para el indicador (mujeres, indígenas y adultos mayores)",
     "APLICACIÓN DEL MÉTODO DE CÁLCULO DEL INDICADOR PARA LA OBTENCIÓN DEL VALOR DE LA LÍNEA BASE":
     {
-        "variables":[
-            {
-                "Nombre variable":"1.- Acciones de financiamiento y subsidios a hombres de municipios indígenas en 2018",
-                "Valor variable": 22958,
-                "Fuente de información variable": "SNIIV",
-            },
-            {
-                "Nombre variable":"2.- Acciones de financiamiento y subsidios a mujeres en 2018",
-                "Valor variable": 489395,
-                "Fuente de información variable": "SNIIV",
-            },
-            {
-                "Nombre variable":"3.- Acciones de financiamiento y subsidios a hombres en municipios indígenas en el rango de edad de 60 o más en 2018",
-                "Valor variable": 21248,
-                "Fuente de información variable": "SNIIV",
-            },
-        ],
-        "Sustitución en método de cálculo del indicador": "Acciones de subsidios y financiamientos de poblaciones vulnerables= 22,958+489,395+21,248"
+        "Nombre variable 1":"1.- Acciones de financiamiento y subsidios a hombres de municipios indígenas en 2018",
+        "Nombre variable 2":"2.- Acciones de financiamiento y subsidios a mujeres en 2018",
+        "Nombre variable 3":"3.- Acciones de financiamiento y subsidios a hombres en municipios no indígenas en el rango de edad de 60 o más en 2018",
+        "Valor variable 1": 22753,
+        "Valor variable 2": 504630,
+        "Valor variable 3": 21863,
+        "Fuente de información variable 1": "SNIIV",
+        "Fuente de información variable 2": "SNIIV",
+        "Fuente de información variable 3": "SNIIV",
+        "Sustitución en método de cálculo del indicador": "Acciones de subsidios y financiamiento de poblaciones vulnerables= 22,753+504,630+21,863"
     },
     "VALOR DE LÍNEA BASE Y METAS":
     {
@@ -54,9 +46,10 @@ export const ind_b3 = {
         "2015":440058,
         "2016":429571,
         "2017":495809,
-        "2018":533601,
-        "2019":436854,
-        "2020":423352,
-        "2021":473703,
+        "2018":549246,
+        "2019":449731,
+        "2020":431413,
+        "2021":484823 
     }
 }
+            
