0a81244404449eef8ed52af196dd6710b039d751
rafaelcanchola <jcancholaa1300@alumno.ipn.mx>
2022-03-14 15:20:52 -0600

    4 trimestre reajuste

--- src/json/PNV/cumplimiento21-4.tsx
+++ src/json/PNV/cumplimiento21-4.tsx
@@ -1,11 +1,3 @@
 export const cumplimientoPNV4 =
-    [
-        {"trimestre":4,"tipoObjetivo":1,"total":36,"concluida":2,"enProceso":33,"sinRealizar":0,"porIniciar":1},
-        {"trimestre":4,"tipoObjetivo":2,"total":30,"concluida":1,"enProceso":22,"sinRealizar":1,"porIniciar":6},
-        {"trimestre":4,"tipoObjetivo":3,"total":12,"concluida":1,"enProceso":9,"sinRealizar":0,"porIniciar":2},
-        {"trimestre":4,"tipoObjetivo":4,"total":7,"concluida":1,"enProceso":5,"sinRealizar":0,"porIniciar":1},
-        {"trimestre":4,"tipoObjetivo":5,"total":28,"concluida":0,"enProceso":22,"sinRealizar":2,"porIniciar":4}
-    ]
-
-
+    [{"trimestre":4,"tipoObjetivo":1,"total":36,"concluida":2,"enProceso":33,"sinRealizar":0,"porIniciar":1},{"trimestre":4,"tipoObjetivo":2,"total":30,"concluida":1,"enProceso":22,"sinRealizar":1,"porIniciar":6},{"trimestre":4,"tipoObjetivo":3,"total":12,"concluida":1,"enProceso":9,"sinRealizar":0,"porIniciar":2},{"trimestre":4,"tipoObjetivo":4,"total":7,"concluida":1,"enProceso":5,"sinRealizar":0,"porIniciar":1},{"trimestre":4,"tipoObjetivo":5,"total":28,"concluida":0,"enProceso":22,"sinRealizar":2,"porIniciar":4}]
 
