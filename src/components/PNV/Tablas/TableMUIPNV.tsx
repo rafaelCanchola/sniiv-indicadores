96a81b015bada5ace3e4610a118fd032271c6d89
rafaelcanchola <jcancholaa1300@alumno.ipn.mx>
2022-06-09 17:01:39 -0500

    fichas avances fix

--- src/components/PNV/Tablas/TableMUIPNV.tsx
+++ src/components/PNV/Tablas/TableMUIPNV.tsx
@@ -57,11 +57,14 @@ export default function TableMUIPNV(props: TableProps) {
 
     const dataKeys = Object.keys(props.data);
     const childKeys = [dataKeys[dataKeys.length-4],dataKeys[dataKeys.length-3],dataKeys[dataKeys.length-2],dataKeys[dataKeys.length-1]];
-    const variablesMap = props.data[childKeys[0]]["variables"];
-    const variableslast = Object.keys(props.data[childKeys[0]])[1];
-    const lineaMap = Object.keys(props.data[childKeys[1]]);
-    const baseMap = Object.keys(props.data[childKeys[1]][lineaMap[0]]);
-    const historicoMap = Object.keys(props.data[childKeys[2]]);
+    const variablesIndex = childKeys[0]==='Observaciones'?1:0
+    const mapIndex = childKeys[0]==='Observaciones'?2:1
+    const historicoIndex = childKeys[0]==='Observaciones'?3:2
+    const variablesMap = props.data[childKeys[variablesIndex]]["variables"];
+    const variableslast = Object.keys(props.data[childKeys[variablesIndex]])[1];
+    const lineaMap = Object.keys(props.data[childKeys[mapIndex]]);
+    const baseMap = Object.keys(props.data[childKeys[mapIndex]][lineaMap[0]]);
+    const historicoMap = Object.keys(props.data[childKeys[historicoIndex]]);
     const classes = useStyles();
     return (
         <Fragment>
@@ -117,7 +120,7 @@ export default function TableMUIPNV(props: TableProps) {
                                 <TableCell className={classes.colHead} align={"center"} colSpan={4}>{dataKeys[14]}</TableCell>
                                 <TableCell className={classes.body} align={"center"} colSpan={5}>{props.data[dataKeys[14]]}</TableCell>
                             </TableRow>
-                            <TableRow><TableCell className={classes.head} align={"center"} colSpan={9}>{childKeys[0]}</TableCell></TableRow>
+                            <TableRow><TableCell className={classes.head} align={"center"} colSpan={9}>{childKeys[variablesIndex]}</TableCell></TableRow>
                             {variablesMap.map((data:any, keys:any) =>
                                 <TableRow key={"Nombre variable " +(keys+1)}>
                                     <TableCell className={classes.colHead} align={"center"} colSpan={2}>{"Nombre variable " +(keys+1)}</TableCell>
@@ -130,31 +133,31 @@ export default function TableMUIPNV(props: TableProps) {
                             )}
                             <TableRow>
                                 <TableCell className={classes.colHead} align={"center"} colSpan={4}>{variableslast}</TableCell>
-                                <TableCell className={classes.body} align={"center"} colSpan={5}>{props.data[childKeys[0]][variableslast]}</TableCell>
+                                <TableCell className={classes.body} align={"center"} colSpan={5}>{props.data[childKeys[variablesIndex]][variableslast]}</TableCell>
                             </TableRow>
-                            <TableRow><TableCell className={classes.head} align={"center"} colSpan={9}>{childKeys[1]}</TableCell></TableRow>
+                            <TableRow><TableCell className={classes.head} align={"center"} colSpan={9}>{childKeys[mapIndex]}</TableCell></TableRow>
                             <TableRow>
                                 <TableCell align={"center"} className={classes.colHead} colSpan={4}>{lineaMap[0]}</TableCell>
                                 <TableCell align={"center"} className={classes.colHead} colSpan={5}>{lineaMap[1]}</TableCell>
                             </TableRow>
                             <TableRow>
                                 <TableCell align={"center"} className={classes.colHead} colSpan={2}>{baseMap[0]}</TableCell>
-                                <TableCell className={classes.body} align={"center"} colSpan={2}>{props.data[childKeys[1]][lineaMap[0]][baseMap[0]].toLocaleString()}</TableCell>
-                                <TableCell className={classes.body} align={"center"} colSpan={5}>{props.data[childKeys[1]][lineaMap[1]]}</TableCell>
+                                <TableCell className={classes.body} align={"center"} colSpan={2}>{props.data[childKeys[mapIndex]][lineaMap[0]][baseMap[0]].toLocaleString()}</TableCell>
+                                <TableCell className={classes.body} align={"center"} colSpan={5}>{props.data[childKeys[mapIndex]][lineaMap[1]]}</TableCell>
                             </TableRow>
                             <TableRow>
                                 <TableCell align={"center"} className={classes.colHead} colSpan={2}>{baseMap[1]}</TableCell>
-                                <TableCell className={classes.body} align={"center"} colSpan={2}>{props.data[childKeys[1]][lineaMap[0]][baseMap[1]]}</TableCell>
+                                <TableCell className={classes.body} align={"center"} colSpan={2}>{props.data[childKeys[mapIndex]][lineaMap[0]][baseMap[1]]}</TableCell>
                             </TableRow>
                             <TableRow>
                                 <TableCell align={"center"} className={classes.colHead} colSpan={4}>{lineaMap[2]}</TableCell>
                                 <TableCell align={"center"} className={classes.colHead} colSpan={5}>{lineaMap[3]}</TableCell>
                             </TableRow>
                             <TableRow>
-                                <TableCell className={classes.body} align={"center"} colSpan={4}>{props.data[childKeys[1]][lineaMap[2]].toLocaleString()}</TableCell>
-                                <TableCell className={classes.body} align={"center"} colSpan={5}>{props.data[childKeys[1]][lineaMap[3]]}</TableCell>
+                                <TableCell className={classes.body} align={"center"} colSpan={4}>{props.data[childKeys[mapIndex]][lineaMap[2]].toLocaleString()}</TableCell>
+                                <TableCell className={classes.body} align={"center"} colSpan={5}>{props.data[childKeys[mapIndex]][lineaMap[3]]}</TableCell>
                             </TableRow>
-                            <TableRow><TableCell align={"center"} className={classes.head} colSpan={9}>{childKeys[2]}</TableCell></TableRow>
+                            <TableRow><TableCell align={"center"} className={classes.head} colSpan={9}>{childKeys[historicoIndex]}</TableCell></TableRow>
                             <TableRow>
 
                                 <TableCell key={'tb0'} className={classes.colHead} align={"center"} colSpan={1}>{""}</TableCell>
@@ -165,7 +168,7 @@ export default function TableMUIPNV(props: TableProps) {
                             <TableRow>
                                 <TableCell key={'tb10'} className={classes.body} align={"center"} colSpan={1}>{""}</TableCell>
                                 {historicoMap.map((data:any, keys:number) =>
-                                    <TableCell key={data+keys} className={classes.body} align={"center"} colSpan={1}>{props.data[childKeys[2]][data].toLocaleString()}</TableCell>
+                                    <TableCell key={data+keys} className={classes.body} align={"center"} colSpan={1}>{props.data[childKeys[historicoIndex]][data].toLocaleString()}</TableCell>
                                 )}
                             </TableRow>
 
